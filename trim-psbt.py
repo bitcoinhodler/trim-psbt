1b43bd5b79a9db928a0f2f9add92477950aa1527
Bitcoin Hodler <bitcoinhodler@safe-mail.net>
2020-07-29 06:06:22 +0000

    Refactor so it can be used as a library module

--- trim-psbt.py
+++ trim-psbt.py
@@ -8,11 +8,8 @@ from bitcoin import psbt
 from binascii import a2b_base64, b2a_base64
 
 
-def main():
-    # Read base64 PSBT from stdin, or file specified on cmdline
-    with fileinput.input() as fin:
-        orig_psbt = next(fin)
-
+def strip(orig_psbt):
+    """Given a base64 PSBT, strip it and return base64 PSBT."""
     # first convert it to binary
     raw = a2b_base64(orig_psbt)
     # then parse
@@ -21,21 +18,22 @@ def main():
     for inp in tx.inputs:
         if inp.witness_utxo and inp.non_witness_utxo:
             inp.non_witness_utxo = None
-
-    print_psbt(tx)
-
-
-def print_psbt(psbt):
-    """Given a PSBT object, write it in base64 to stdout."""
-    raw = psbt.serialize()
+    rawout = tx.serialize()
     # convert to base64
-    b64_psbt = b2a_base64(raw)
+    b64_psbt = b2a_base64(rawout)
     # somehow b2a ends with \n...
     if b64_psbt[-1:] == b"\n":
         b64_psbt = b64_psbt[:-1]
     # print
     new_psbt = b64_psbt.decode('utf-8')
-    print(new_psbt)
+    return new_psbt
+
+
+def main():
+    # Read base64 PSBT from stdin, or file specified on cmdline
+    with fileinput.input() as fin:
+        orig_psbt = next(fin)
+    print(strip(orig_psbt))
 
 
 if __name__ == '__main__':
